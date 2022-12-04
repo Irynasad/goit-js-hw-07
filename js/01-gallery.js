d9da6bdb1a9bb2cf5a3cfae909dbbdbb580935b4
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-04 13:07:10 +0200

    Update 1tast
    удален лишний код, перенесена реализация закрития модалки клавишей Esc на лайтбокс

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 
-const modal = basicLightbox.create(`
-    <img class="modal-img" src='https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg' alt="img"/>
-`);
+const modal = basicLightbox.create(
+  `
+  <img class="modal-img" src='https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg' alt="img"/>
+  `,
+  {
+    onShow: (modal) => {
+      document.addEventListener("keydown", onCloseModalKeydown);
+    },
+    onclose: (modal) => {
+      document.removeEventListener("keydown", onCloseModalKeydown);
+    },
+  }
+);
 
 const containerGallery = document.querySelector(".gallery");
-
 const listOfGallery = createGallery(galleryItems);
 
 containerGallery.insertAdjacentHTML("beforeend", listOfGallery);
-
 containerGallery.addEventListener("click", onImgOriginal);
 
 function onImgOriginal(event) {
   event.preventDefault();
-  window.addEventListener("keydown", onCloseModalKeydown);
 
   if (!event.target.classList.contains("gallery__image")) {
     return;
@@ -24,13 +31,6 @@ function onImgOriginal(event) {
   const descrImage = event.target.getAttribute("alt");
 
   viewOriginalSizeGalleryItem(urlImage, descrImage);
-
-  removeActiveClassOfGalleryItem();
-
-  const galleryItem = event.target;
-  const parentGaleryItem = galleryItem.closest(".gallery__item");
-
-  addActiveClassOffGalleryItem(parentGaleryItem);
 }
 
 function createGallery(items) {
@@ -50,17 +50,6 @@ function createGallery(items) {
     .join(" ");
 }
 
-function removeActiveClassOfGalleryItem() {
-  const currentGalleryItem = document.querySelector(".gallery__item.is-active");
-  if (currentGalleryItem) {
-    currentGalleryItem.classList.remove("is-active");
-  }
-}
-
-function addActiveClassOffGalleryItem(item) {
-  item.classList.add("is-active");
-}
-
 function viewOriginalSizeGalleryItem(url, description) {
   const modalImage = modal.element().querySelector(".modal-img");
   modalImage.setAttribute("src", url);
@@ -70,8 +59,6 @@ function viewOriginalSizeGalleryItem(url, description) {
 
 function onCloseModalKeydown(event) {
   if (event.code === "Escape") {
-    modal.close(() => {
-      window.removeEventListener("keydown", onCloseModalKeydown);
-    });
+    modal.close();
   }
 }
